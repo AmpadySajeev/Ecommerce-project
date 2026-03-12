3fbb5025200205d55d8e7ff6f0ee8e103495010b
AmpadySajeev <ampadysajeev05@gmail.com>
2026-03-12 12:44:34 +0530

    fixed staticfiles storage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -112,7 +112,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
